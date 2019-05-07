7a8fd7a2edf74ff05114ccf31f71379bf8ae4009
Logan Campos <logan.campos123@gmail.com>
2019-05-07 12:39:38 -0500

    Grammar and punctuation in comments

--- anchors/anchor_repository.go
+++ anchors/anchor_repository.go
@@ -9,19 +9,19 @@ import (
 
 var log = logging.Logger("anchorRepository")
 
-// AnchorRepository defines a set of functions that can be
-// implemented by any type that stores and retrieves the anchoring, and pre anchoring details.
+// AnchorRepository defines a set of functions that enable
+// implementations of any type that stores and retrieves the anchoring, and pre-anchoring details.
 type AnchorRepository interface {
 
-	// PreCommitAnchor will call the transaction PreCommit on the smart contract, to pre commit a document update
+	// PreCommitAnchor calls a transaction's PreCommit on the smart contract, to pre-commit a document update.
 	PreCommitAnchor(ctx context.Context, anchorID AnchorID, signingRoot DocumentRoot) (confirmations chan bool, err error)
 
-	// CommitAnchor will send a commit transaction to Ethereum.
+	// CommitAnchor sends a Commit transaction to Ethereum.
 	CommitAnchor(ctx context.Context, anchorID AnchorID, documentRoot DocumentRoot, proof [32]byte) (chan bool, error)
 
 	// GetAnchorData takes an anchorID and returns the corresponding documentRoot from the chain.
 	GetAnchorData(anchorID AnchorID) (docRoot DocumentRoot, anchoredTime time.Time, err error)
 
-	// HasValidPreCommit checks if the given anchorID has a valid pre-commit
+	// HasValidPreCommit checks if the given anchorID has a valid pre-commit.
 	HasValidPreCommit(anchorID AnchorID) bool
 }
